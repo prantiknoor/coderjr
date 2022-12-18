43bbfb4a4561d41b3193fa5a6a61949eace2d211
Prantik <prantiknoor@gmail.com>
2022-12-18 09:50:07 +0600

    first commit

---
+++ extension.js
@@ -0,0 +1,15 @@
+const vscode = require("vscode");
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  // context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
